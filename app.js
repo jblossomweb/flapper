fcb1d1977ecaf28092a13485950ef7de45d9ed56
John Blossom <jblossom.web@gmail.com>
2015-08-05 11:44:35 -0400

    prevent blank submits

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function($scope){
 	  {title: 'post 5', upvotes: 4}
 	]
 	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === '') { return }
 	  $scope.posts.push({title: $scope.title, upvotes: 0})
 	  $scope.title = ''
 	}
