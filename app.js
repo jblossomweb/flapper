e8ea1fcccc3bccd27df1f0c2b3f0abc80856218f
John Blossom <jblossom.web@gmail.com>
2015-08-05 11:39:36 -0400

    mock addPost method

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ function($scope){
 	  {title: 'post 4', upvotes: 9},
 	  {title: 'post 5', upvotes: 4}
 	]
+	$scope.addPost = function(){
+	  $scope.posts.push({title: 'A new post!', upvotes: 0})
+	}
 }])
