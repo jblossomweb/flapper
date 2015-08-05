af06c9d5e736c3049ea1e7594aa76441b079d3ec
John Blossom <jblossom.web@gmail.com>
2015-08-05 11:31:35 -0400

    mock posts

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello world!'
+  $scope.posts = [
+	  'post 1',
+	  'post 2',
+	  'post 3',
+	  'post 4',
+	  'post 5'
+	]
 }])
