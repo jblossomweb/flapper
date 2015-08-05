86f538918195e73ab53e99ebd8865f56a71879a6
John Blossom <jblossom.web@gmail.com>
2015-08-05 11:33:09 -0400

    replace test with posts

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = angular.module('flapperNews', [])
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!'
   $scope.posts = [
 	  'post 1',
 	  'post 2',
