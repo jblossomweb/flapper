99cca2fabb6432d1ffae7f8654fd88da92d077a0
John Blossom <jblossom.web@gmail.com>
2015-08-05 11:34:52 -0400

    refactor posts data structure

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.posts = [
-	  'post 1',
-	  'post 2',
-	  'post 3',
-	  'post 4',
-	  'post 5'
+		{title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
 	]
 }])
