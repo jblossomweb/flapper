208c5100999c1d7c196539603e5c0e7895c2aa59
John Blossom <jblossom.web@gmail.com>
2015-08-05 12:05:01 -0400

    relocate mock data to factory

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var app = angular.module('flapperNews', [])
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.posts = [
+app.factory('posts', [function(){
+  return [
 		{title: 'post 1', upvotes: 5},
 	  {title: 'post 2', upvotes: 2},
 	  {title: 'post 3', upvotes: 15},
 	  {title: 'post 4', upvotes: 9},
 	  {title: 'post 5', upvotes: 4}
 	]
+}])
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope,posts){
+  $scope.posts = posts
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === '') { return }
 	  $scope.posts.push({
